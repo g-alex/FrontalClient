2e74d041f989460cb547799f36eb4c2d3eb40fa5
Alexandre Garnier <alexandre.garnier.86@gmail.com>
2011-01-03 13:42:25 +0100

    Final commit ?

--- src/java/frontalclient/MainClient.java
+++ src/java/frontalclient/MainClient.java
@@ -8,15 +8,19 @@ import fr.alma.client.Client;
 import fr.alma.client.ClientBeanRemote;
 import fr.alma.dto.catalogue.Categorie;
 import fr.alma.dto.central.AProduit;
+import fr.alma.dto.central.CProduit;
 import fr.alma.dto.central.Item;
 import fr.alma.interfaces.CentralRemote;
 import fr.alma.order.OrderBeanRemote;
+import fr.alma.order.Ordering;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
@@ -46,7 +50,7 @@ public class MainClient {
         System.out.println("* 0. Quitter                  *");
         System.out.println("*******************************");
         System.out.print("?: ");
-        return in.read() - 48;
+        return Integer.parseInt(in.readLine());
     } // int mainMenu()
 
     private static Categorie orderMenu(Collection<Categorie> categs)
@@ -76,10 +80,11 @@ public class MainClient {
         System.out.println("* 0. Terminer                          *");
         System.out.println("****************************************");
         System.out.print("?: ");
-        return in.read() - 48;
+        return Integer.parseInt(in.readLine());
     } // int scanCategMenu(Categorie)
 
-    private static AProduit pickItemsMenu(Collection<AProduit> items) throws IOException {
+    private static AProduit pickItemsMenu(Collection<AProduit> items)
+            throws IOException {
         System.out.println("  \tMarque\tModele\tDescription\n");
         int i = 0;
         List<AProduit> itemsList = new ArrayList<AProduit>();
@@ -90,21 +95,32 @@ public class MainClient {
             itemsList.add(item);
         } // for
         System.out.println("0. Terminer");
-        System.out.println("\nSelectionnez le numero d'un produit pour l'ajouter au panier");
+        System.out.println("\nSelectionnez le numero d'un produit pour "
+                + "l'ajouter au panier");
         System.out.print("?: ");
         return itemsList.get(Integer.parseInt(in.readLine()));
-    }
+    } // AProduit pickItemsMenu(Collection<AProduit>)
 
     private static int addItemMenu(AProduit item) throws IOException {
         System.out.println("Etes-vous sur ? (1 pour oui, 0 pour non)");
         System.out.print("?: ");
-        return in.read() - 48;
-    }
+        return Integer.parseInt(in.readLine());
+    } // int addItemMenu(AProduit)
 
-    private static Item providerMenu(AProduit item) {
-        // TODO
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+    private static Item selectProviderMenu(List<CProduit> produits,
+            AProduit item)
+            throws IOException {
+        System.out.println("Veuillez selectionner un fournisseur");
+        System.out.println("   \tFournisseur\tPrix");
+        int i = 0;
+        for (CProduit produit : produits) {
+            System.out.println(++i + ".\t" + produit.getFournisseur() + "\t"
+                    + produit.getPrix() + "euros");
+        } // for
+        System.out.print("?: ");
+        return CProduitToItem(produits.get(Integer.parseInt(in.readLine()) - 1),
+                item);
+    } // Item selectProviderMenu(List<CProduit>, AProduit)
 
     private static void order()
             throws NamingException, IOException {
@@ -157,39 +173,66 @@ public class MainClient {
         } // while
     } // void scanCateg(String[],Categorie)
 
-    private static void scanItems(Categorie categ, String brand, Integer minPrice, Integer maxPrice)
+    private static void scanItems(Categorie categ, String brand,
+            Integer minPrice, Integer maxPrice)
             throws IOException {
         if (brand != null) {
             if (minPrice != null && maxPrice != null) {
-                pickItems(centralService.findByCategorieAndMarqueAndPriceRange(categ.getName(), brand, minPrice.doubleValue(), maxPrice.doubleValue()));
+                pickItems(centralService.findByCategorieAndMarqueAndPriceRange(categ.getName(),
+                        brand, minPrice.doubleValue(), maxPrice.doubleValue()));
             } else {
-                pickItems(centralService.findByCategorieAndMarque(categ.getName(), brand));
+                pickItems(centralService.findByCategorieAndMarque(categ.getName(),
+                        brand));
             } // if
         } else {
             if (minPrice != null && maxPrice != null) {
-                pickItems(centralService.findByCategorieAndPriceRange(categ.getName(), minPrice.doubleValue(), maxPrice.doubleValue()));
+                pickItems(centralService.findByCategorieAndPriceRange(categ.getName(),
+                        minPrice.doubleValue(), maxPrice.doubleValue()));
             } else {
                 pickItems(centralService.findProduitsByCategorie(categ.getName()));
             } // if
         } // if
     } // void items(Categorie,String,Integer,Integer)
 
-    private static void pickItems(Collection<AProduit> items) throws IOException {
+    private static void pickItems(Collection<AProduit> items)
+            throws IOException {
         while (true) {
             AProduit item = pickItemsMenu(items);
             if (item != null) {
                 if (addItemMenu(item) != 0) {
                     if (orderId == null) {
+                        orderId = orderService.createOrder(client.getName(),
+                                client.getAdress());
                     }
                     System.out.print("Quantite : ");
-                    orderService.addProduct(orderId, providerMenu(item),
-                            Integer.parseInt(in.readLine()));
-                }
+                    Integer quantity = Integer.parseInt(in.readLine());
+                    orderService.addProduct(orderId, selectProvider(item),
+                            quantity);
+                } // if
             } else {
                 return;
             } // if
         } // while
     } // void scanItemMenu(Collection<AProduit>)
+
+    private static Item selectProvider(AProduit item) throws IOException {
+        List<CProduit> produits = item.getProduitFournis();
+        if (produits.isEmpty()) {
+            return null;
+        } // if
+        if (produits.size() == 1) {
+            return CProduitToItem(produits.get(0), item);
+        } // if
+        return selectProviderMenu(produits, item);
+    } // Item selectProvider(AProduit)
+
+    private static Item CProduitToItem(CProduit cprod, AProduit aprod) {
+        Item item = new Item();
+        item.setFournisseur(cprod.getFournisseur());
+        item.setMarque(aprod.getMarque());
+        item.setModel(aprod.getModele());
+        return item;
+    } // Item CProduitToItem(CProduit, AProduit)
 
     /**
      * @param args the command line arguments
@@ -207,7 +250,7 @@ public class MainClient {
                         String pwd = in.readLine();
                         client = clientService.getClient(name, pwd);
                         break;
-                    }
+                    } // case 1
                     case 2: { // Register
                         System.out.print("Nom : ");
                         String name = in.readLine();
@@ -215,21 +258,30 @@ public class MainClient {
                         String adress = in.readLine();
                         System.out.print("Mot de passe : ");
                         String pwd = in.readLine();
-                        client = clientService.getClient(clientService.createClient(name, adress, pwd));
+                        client = clientService.getClient(clientService.createClient(name,
+                                adress, pwd));
                         break;
-                    }
+                    } // case 2
                     default:
                         return;
                 } // switch
             } else {
                 order();
-                pay();
+                printOrder();
                 return;
             } // if
         } // while
     } // void main(String[])
 
-    private static void pay() {
-        throw new UnsupportedOperationException("Not yet implemented");
-    }
+    private static void printOrder() {
+        Ordering order = orderService.getOrder(orderId);
+        HashMap<Item, Integer> items = order.getProducts();
+        System.out.println("Marque\tModele\tQuantite");
+        for (Map.Entry<Item, Integer> entry : items.entrySet()) {
+            Item item = entry.getKey();
+            Integer quantity = entry.getValue();
+            System.out.println(item.getMarque() + "\t" + item.getModel() + "\t"
+                    + quantity);
+        } // for
+    } // void pay()
 }
